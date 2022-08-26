f88e8b47ac7e5f07fb8e903b993bba61be9ef63b
buhariaminat <buhariaminat@yahoo.com>
2022-08-26 01:41:30 +0100

    Add greetings to database

---
+++ db/seeds.rb
@@ -0,0 +1,17 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+#   Character.create(name: "Luke", movie: movies.first)
+greetings = Greeting.create([
+  { message: 'Hi' },
+  { message: 'Holla' },
+  { message: 'Buenos días' },
+  { message: 'Ekaaro' },
+  { message: 'Good morning' },
+  { message: 'Hey' },
+  { message: 'Yo' },
+  { message: 'Asalaam Alaykum' },
+])
